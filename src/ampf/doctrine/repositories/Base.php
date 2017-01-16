c4c1cc7a275feff9593913f1eb42096cc1515605
Rene Pasing <rene@pasing.net>
2017-01-16 01:52:55 +0100

    Add a Doctrine repository method to do a DQL bulk remove operation.

--- src/ampf/doctrine/repositories/Base.php
+++ src/ampf/doctrine/repositories/Base.php
@@ -8,6 +8,39 @@ use \ampf\doctrine\entities\Base as BaseEntity;
 abstract class Base extends EntityRepository
 {
 	/**
+	 * @param array $criteria
+	 * @return int Affected rows
+	 * @throws \Exception
+	 */
+	public function bulkRemoveBy(array $criteria)
+	{
+		if (count($criteria) < 1)
+		{
+			throw new \Exception("You may not use this method to truncate a whole table.");
+		}
+
+		$qb = $this->_em->createQueryBuilder();
+		$qb->delete($this->getClassName(), 't');
+
+		$where = $qb->expr()->andX();
+		foreach ($criteria as $key => $value)
+		{
+			if (!is_string($key) || trim($key) == '')
+			{
+				throw new \Exception("criteria keys must always be strings");
+			}
+			$where->add($qb->expr()->eq(
+				('t.' . $key),
+				(':' . $key)
+			));
+			$qb->setParameter($key, $value);
+		}
+		$qb->where($where);
+
+		return $qb->getQuery()->execute();
+	}
+
+	/**
 	 * @return BaseEntity
 	 */
 	public function create()
